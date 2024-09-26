fa993dfafeafff78aa36f4371fcccfe471f89d06
Sujithkumarvelpula <113710355+Sujithkumarvelpula@users.noreply.github.com>
2024-09-27 00:52:50 +0530

    first commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const moragan = require("morgan");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-
+const path=require("path")
 //dotenv conig
 
 dotenv.config();
@@ -24,6 +24,11 @@ app.use("/api/v1/user", require("./routes/userRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
 
+//static files
+app.use(express.static(path.join(__dirname,"./client/build")));
+app.get('*',function (req,res) {
+  res.sendFile(path.join(__dirname,"./client/build/index.html"));
+});
 //port
 const port = process.env.PORT || 8000;
 //listen port
